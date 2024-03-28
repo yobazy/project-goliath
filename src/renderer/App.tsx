cdda0eb0e7e0aec663f64438b2f848c076cc1663
Bazil <bazil.khan@urbaneyes.ai>
2024-03-28 19:37:48 -0400

    Removes donate button

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,7 +8,7 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1>PROJECT-DESTROY</h1>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
@@ -20,18 +20,6 @@ function Hello() {
               📚
             </span>
             Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
           </button>
         </a>
       </div>
